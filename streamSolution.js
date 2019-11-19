92c93deb80fb4396b8135a3760b9eaa43d289e9b
Marius <dohmen@icho-systems.de>
2019-11-19 20:47:28 +0100

    working better now!

--- streamSolution.js
+++ streamSolution.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+const { Transform } = require('stream');
 const { StringStream } = require("scramjet");
 
 let lines_count = 0;
@@ -7,21 +8,28 @@ const transformToMLSSA = function(line) {
 		if(lines_count > 600)
 			process.exit();
 		console.log({line},{lines_count});
+		return line+"abc"
 }
 
-const runStream = async function() {
+const map = (fn, options = {}) => new Transform({
+  // By default we are in object mode but this can be overwritten by the user
+  objectMode: true,
+  ...options,
+  
+  transform(chunk, encoding, callback) {
+    let res;
+    try {
+      res = fn(chunk);
+    } catch(e) {
+      return callback(e);
+    }
+    callback(null, res);
+  }
+})
 
-	let readStream = fs.createReadStream('./DataFiles/Online_Speaker_Data/ND25FA-4@0.frd')
-	try {
-	     readStream.pipe(new StringStream('utf-8'))
-	     .split('\r\n')                                          // split every line
-	     .map(async (line) => line+="1")    // update the lines
-	     .join('\r\n')                                           // join again
-	     .pipe(fs.createWriteStream('./ConvertedFiles/myfile.txt'));
-	 } catch(err) {
-	 	console.log({err});
-	 }
-	
-}
-
-runStream();
+fs.createReadStream('./DataFiles/Online_Speaker_Data/ND25FA-4@0.frd')
+	.pipe(new StringStream('utf-8'))
+	.split('\r\n')
+	.pipe(map(transformToMLSSA))
+	.split('\t')
+	.pipe(map((element)=>{console.log({element})}))
